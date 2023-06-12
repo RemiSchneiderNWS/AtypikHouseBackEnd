c21e3f98c43ca3218088d190d8cf8fce852fd12b
RemiSchneiderNWS <rschneide@normandiewebschool.fr>
2023-06-12 17:43:05 +0200

    Ajourt du model concernant les commentaires

---
+++ AtypikHouseBackEnd/Models/Commentarys.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AtypikHouseBackEnd.Models
+{
+    [Table("commentarys")]
+    public class Commentarys
+    {
+        [Key]
+        public int com_id { get; set; }
+        public string com_text { get; set; }
+        public int com_adv_id { get; set; }
+        public int com_usr_id { get; set; }
+        public string com_usr_firstName { get; set; }
+        public string com_usr_lastName { get; set; }
+    }
+}
