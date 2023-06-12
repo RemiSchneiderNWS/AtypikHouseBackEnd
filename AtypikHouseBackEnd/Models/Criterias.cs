8981ea121fc01134455ca7e3059584ad2dd256db
RemiSchneiderNWS <rschneide@normandiewebschool.fr>
2023-06-12 17:50:42 +0200

    ajout model concernant les critéres

---
+++ AtypikHouseBackEnd/Models/Criterias.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AtypikHouseBackEnd.Models
+{
+    [Table("criteria")]
+    public class Criterias
+    {
+        [Key]
+        public int cri_id { get; set; }
+        public string cri_name { get; set; }
+
+    }
+}
