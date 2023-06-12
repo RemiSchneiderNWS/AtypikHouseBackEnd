20c0d48a3682c6c116e73449a5b8d8ad0ee0ec19
RemiSchneiderNWS <rschneide@normandiewebschool.fr>
2023-06-12 18:00:11 +0200

    Ajourt de la Table de relation en critére et advert

---
+++ AtypikHouseBackEnd/Models/criteriadvert.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AtypikHouseBackEnd.Models
+{
+    [Table("criteriadvert")]
+    public class criteriadvert
+    {
+         [Key] 
+         public int ria_id { get; set; }
+         public int ria_cri_id { get; set; } 
+         public int ria_adv_id { get; set; }
+
+    }
+}
