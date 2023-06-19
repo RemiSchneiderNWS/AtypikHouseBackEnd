9f13227403822c613d8447964f2f7e544a8b2dbf
RemiSchneiderNWS <rschneide@normandiewebschool.fr>
2023-06-19 22:41:21 +0200

    Création de l'user controller

---
+++ AtypikHouseBackEnd/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Domain;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Repositories;
+using AtypikHouseBackEnd.Services;
+using System.Text.RegularExpressions;
+
+namespace AtypikHouseBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly UserRepository User; 
+        
+        public UserController(UserRepository user)
+        {
+            User = user;
+        }
+
+        [HttpPost]
+        public bool CreateUser(User user)
+        {
+            string passwordHashed = Hashing.Sha512(user.Password);
+            user.Password = passwordHashed;
+           /* var emailRegex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+            if (!emailRegex.IsMatch(user.Mail))
+            {
+
+                return BadRequest("L'adresse e-mail n'est pas au bon format.");
+            }*/
+            User.Add(user);
+            User.Save();
+            return true;
+        }
+
+    }
+}
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -15,4 +15,6 @@ public class UserRepository : Repository<User>
     {
         Set = ctx.Users;
     }
+
+   
 }
