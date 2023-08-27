e504875cc8335351cca8409b9f1318757a622ed7
RemiSchneiderNWS <rschneide@normandiewebschool.fr>
2023-08-27 11:01:22 +0200

    Insert badUSer

--- AtypikHouseBackEnd/UnitTests/UserControllerTests.cs
+++ AtypikHouseBackEnd/UnitTests/UserControllerTests.cs
@@ -24,10 +24,29 @@ public class UserControllerTests
             Password = "1234Azerty___",
             Phone = "0612234556", 
             FirstName = "Rémi",
-            LastName = "Scheinder"
+            LastName = "Schneider"
         });
 
         Assert.IsTrue(response.Value);
         Assert.AreEqual(1, application.Services.GetService<UserRepository>()!.All().Count());
+    }
+    [TestMethod]
+    public void CreatBadUser()
+    {
+        WebApplication application = TestsUtils.CreateContext();
+        UserController controller = application.Services.GetService<UserController>()!;
+
+        Assert.AreEqual(0, application.Services.GetService<UserRepository>()!.All().Count());
+        ActionResult<bool> response = controller.CreateUser(new CreateUserPayload
+        {
+            Mail = "remi.fr",
+            Password = "1234azerty___",
+            Phone = "0612234556",
+            FirstName = "Rémi",
+            LastName = "Schneider"
+        });
+
+        Assert.IsFalse(response.Value);
+        Assert.AreEqual(0, application.Services.GetService<UserRepository>()!.All().Count());
     }
 }
