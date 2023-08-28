c1822a1a0290c416e3f3271febbda37d52887dd4
Rémi Schneider <remi.s76600@gmail.com>
2023-08-28 14:19:58 +0200

    Fix migration

--- AtypikHouseBackEnd/AtypikHouseBackEnd/Program.cs
+++ AtypikHouseBackEnd/AtypikHouseBackEnd/Program.cs
@@ -2,12 +2,26 @@ using System.Text;
 using AtypikHouseBackEnd;
 using Database;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Repositories;
 
+bool migration = false;
+
 WebApplication app = ProjectApplicationBuilder.CreateProjectApp(args, new Action<WebApplicationBuilder>[]
 {
-    i => i.Services.AddScoped<AppDbContext, PGSQLAppDbContext>(o => new PGSQLAppDbContext(o.GetService<IConfiguration>()!["ConnectionStrings:Connection"] ?? null))
+    i => i.Services.AddScoped<AppDbContext, PGSQLAppDbContext>(o =>
+    {
+        PGSQLAppDbContext connection = new PGSQLAppDbContext(o.GetService<IConfiguration>()!["ConnectionStrings:Connection"] ?? null);
+
+        if (!migration)
+        {
+            migration = true;
+            connection.Database.Migrate();
+        }
+
+        return (connection);
+    })
 });
 
 app.Run();
